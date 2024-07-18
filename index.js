169b5ea5146aa77b06a4597dbe5978a54ce57051
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-07-18 19:15:20 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) { // Assuming the dodger container width is 400px and dodger width is 40px
+        dodger.style.left = `${left + 10}px`;
+    }
+}
